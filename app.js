4964bbf8e80903f4a6591ae107d46d12eb1fb109
LizardLove <1026123335@qq.com>
2017-11-07 21:08:39 +0800

    littel.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 * @Author: 10261
 * @Date:   2017-11-06 22:46:53
 * @Last Modified by:   10261
-* @Last Modified time: 2017-11-06 23:05:54
+* @Last Modified time: 2017-11-07 21:08:25
 */
 var express = require('express');
 var app = express();
 
 app.use(express.static("./public"));
 
-app.set('port', (process.env.PORT || 9999));
+app.set('port', (process.env.PORT || 8888));
 
 app.listen(app.get('port'), function () {
 	console.log('run in ', app.get('port'));
